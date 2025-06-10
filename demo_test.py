c6e3dd5760bfa2cd32bc5171d5786a5027bec21f
jacekkulma <jacek.kulma@outlook.com>
2025-06-10 17:45:43 +0200

    main func def

--- demo_test.py
+++ demo_test.py
@@ -19,23 +19,27 @@ test_functions = {
 seeds = [42, 1234, 987654, 20240406, 777, 314159, 8675309, 99999, 1337, 55555]
 
 ## tests
-for algorithm in algorithms:
-    print(f"Running algorithm: {algorithm.__name__}\n")
-    for name, func in test_functions.items():
-        print(f" Testing function: {name}")
+def main() -> None:
+    for algorithm in algorithms:
+        print(f"Running algorithm: {algorithm.__name__}\n")
+        for name, func in test_functions.items():
+            print(f" Testing function: {name}")
 
-        function_values = []
-        best_solutions = []
-        for seed in seeds:
-            np.random.seed(seed)
-            x0 = np.random.uniform(-5, 5, size=dim)
-            result = algorithm(func, x0, sigma, population_size, max_iter)
-            final_value = func(result)
-            
-            function_values.append(final_value)
-            best_solutions.append(result)
+            function_values = []
+            best_solutions = []
+            for seed in seeds:
+                np.random.seed(seed)
+                x0 = np.random.uniform(-5, 5, size=dim)
+                result = algorithm(func, x0, sigma, population_size, max_iter)
+                final_value = func(result)
+                
+                function_values.append(final_value)
+                best_solutions.append(result)
 
-        avg_value = np.mean(function_values)
-        avg_best_solution = np.mean(best_solutions, axis=0)
-        print(f"   Best solution: {avg_best_solution}")
-        print(f"   Objective function value: {avg_value:.6f}\n")
+            avg_value = np.mean(function_values)
+            avg_best_solution = np.mean(best_solutions, axis=0)
+            print(f"   Best solution: {avg_best_solution}")
+            print(f"   Objective function value: {avg_value:.6f}\n")
+
+if __name__ == "__main__":
+    main()
--- get_algorithms_results.py
+++ get_algorithms_results.py
@@ -23,32 +23,36 @@ test_functions = {
 seeds = [42, 1234, 987654, 20240406, 777, 314159, 8675309, 99999, 1337, 55555]
 
 ## tests
-for dim in dims:
-    for name, func in test_functions.items():   
-        # create output dir
-        dir = os.path.join(output, f"dim_{dim}", f"{name}")
-        os.makedirs(dir, exist_ok=True)
-        for algorithm in algorithms:
-            function_values = []
-            best_solutions = []
-            for seed in seeds:
-                np.random.seed(seed)
-                x0 = np.random.uniform(-5, 5, size=dim)
-                result = algorithm(func, x0, sigma, population_size, max_iter, seed)
-                final_value = func(result)
-                
-                function_values.append(final_value)
-                best_solutions.append(result)
+def main() -> None:
+    for dim in dims:
+        for name, func in test_functions.items():   
+            # create output dir
+            dir = os.path.join(output, f"dim_{dim}", f"{name}")
+            os.makedirs(dir, exist_ok=True)
+            for algorithm in algorithms:
+                function_values = []
+                best_solutions = []
+                for seed in seeds:
+                    np.random.seed(seed)
+                    x0 = np.random.uniform(-5, 5, size=dim)
+                    result = algorithm(func, x0, sigma, population_size, max_iter, seed)
+                    final_value = func(result)
+                    
+                    function_values.append(final_value)
+                    best_solutions.append(result)
 
-            avg_value = np.mean(function_values)
-            avg_best_solution = np.mean(best_solutions, axis=0)
+                avg_value = np.mean(function_values)
+                avg_best_solution = np.mean(best_solutions, axis=0)
 
-            # save results for algorithm, dim and function (avg from all seeds)
-            file_path = os.path.join(dir, f"{algorithm.__name__}.txt")
-            with open(file_path, "w") as f:
-                f.write(f"algorithm: {algorithm.__name__}, dimension: {dim}, function: {name}\n")
-                f.write(f"{avg_value:.6f}\n")
-                f.write(f"{np.round(avg_best_solution, 6)}\n")
-            print(f"Results - algorithm: {algorithm.__name__}, dimension: {dim}, function: {name} - saved.")
+                # save results for algorithm, dim and function (avg from all seeds)
+                file_path = os.path.join(dir, f"{algorithm.__name__}.txt")
+                with open(file_path, "w") as f:
+                    f.write(f"algorithm: {algorithm.__name__}, dimension: {dim}, function: {name}\n")
+                    f.write(f"{avg_value:.6f}\n")
+                    f.write(f"{np.round(avg_best_solution, 6)}\n")
+                print(f"Results - algorithm: {algorithm.__name__}, dimension: {dim}, function: {name} - saved.")
 
-print("\nALL DONE.")
+    print("\nALL DONE.")
+
+if __name__ == "__main__":
+    main()
